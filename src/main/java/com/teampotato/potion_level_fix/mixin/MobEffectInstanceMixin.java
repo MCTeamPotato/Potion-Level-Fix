fcf371ef45ef2b9d69fb87fad1acb345d80b360d
KL_jiana <2436262516@qq.com>
2024-07-18 14:14:25 +0800

    hashmap clean

--- src/main/java/com/teampotato/potion_level_fix/mixin/MobEffectInstanceMixin.java
+++ src/main/java/com/teampotato/potion_level_fix/mixin/MobEffectInstanceMixin.java
@@ -5,32 +5,35 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Mixin(MobEffectInstance.class)
 public abstract class MobEffectInstanceMixin {
     @Shadow
     private int amplifier;
-    @Shadow public abstract String getDescriptionId();
+    @Shadow
+    public abstract String getDescriptionId();
+    @Shadow
+    private int duration;
 
-    @Shadow private int duration;
 
-    @Shadow @Nullable private MobEffectInstance hiddenEffect;
+    @Shadow public abstract Optional<MobEffectInstance.FactorData> getFactorData();
 
-    @Shadow abstract void setDetailsFrom(MobEffectInstance pEffectInstance);
+    @Shadow @Final private Optional<MobEffectInstance.FactorData> factorData;
 
-    @Redirect(method = "writeDetailsTo", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;putByte(Ljava/lang/String;B)V"))
-    private void redirectPutByte(CompoundTag pNbt, String key, byte value) {
+    @Inject(method = "writeDetailsTo", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;putByte(Ljava/lang/String;B)V"))
+    private void redirectPutByte(CompoundTag pNbt, CallbackInfo ci) {
         pNbt.putInt("PLF:Amplifier", this.amplifier);
     }
 
@@ -39,14 +42,14 @@ public abstract class MobEffectInstanceMixin {
         return pNbt.getInt("PLF:Amplifier");
     }
 
-    @Inject(method = "tick", at = @At("HEAD"))
+    @Inject(method = "tick", at = @At("RETURN"))
     private void sentAmplifier(LivingEntity pEntity, Runnable pOnExpirationRunnable, CallbackInfoReturnable<Boolean> cir) {
         Map<String, Integer> map = new HashMap<>();
         map.put(pEntity.getStringUUID(), this.amplifier);
-        PotionLevelFix.PLFAmplifier.put(this.getDescriptionId(), map);
-
-        if (this.duration == 0) {
-            map.remove(pEntity.getStringUUID());
+        if (this.duration > 0) {
+            PotionLevelFix.PLFAmplifier.put(this.getDescriptionId(), map);
+        } else {
+            PotionLevelFix.PLFAmplifier.remove(this.getDescriptionId(), map);
         }
     }
 }
